5f6a87b178544d7bc3f6406e1dde1c16062ea180
christianruizj <37563537+christianruizj@users.noreply.github.com>
2018-04-21 13:45:40 -0300

    Ahora suma 5 números

--- suma.py
+++ suma.py
@@ -1,4 +1,4 @@
 Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> def suma(a,b,c,d):
-	return a+b+c+d
+>>> def suma(a,b,c,d,e):
+	return a+b+c+d+e
